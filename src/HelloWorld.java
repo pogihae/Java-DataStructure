fc8db7c27942078d2f847372e10a64ca7e318b0e
PushStone <76048647+pogihae@users.noreply.github.com>
2021-06-18 11:12:23 +0900

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<15; i++) {
+			stack.push(i);
+		}
+		
+		System.out.println(stack);
+		
+		for(int i=0; i<15; i++) {
+			System.out.print(stack.peek() +" ");
+			stack.pop();
+
+		}
+		
+		System.out.println(stack);
+		
+	}
+
+}
---
+++ src/Stack.java
@@ -0,0 +1,76 @@
+import java.util.NoSuchElementException;
+import List.LinkedList;
+
+/**
+ * @author tir29
+ *
+ * @param <E> type
+ */
+public class Stack<E> {
+	
+	private static final int TOP = 0;
+	
+	private LinkedList<E> stack;
+	
+	/*
+	 * constructor
+	 * 
+	 * make linkedlist
+	 */
+	public Stack() {
+		stack = new LinkedList<>();
+	}
+	
+	
+	/**
+	 * 
+	 * add data to stack on top
+	 * 
+	 * @param data to push
+	 */
+	public void push(E e) {
+		stack.add(e);
+	}
+	
+	/**
+	 * 
+	 * get top and remove it
+	 * 
+	 * @return top of data
+	 * @throws NoSuchElementException by peek
+	 */
+	public E pop() {
+		E data = peek();
+		stack.remove(data);
+		
+		return data;
+	}
+	
+	/**
+	 * 
+	 * just peek top, no remove it
+	 * 
+	 * @return top of data
+	 * @throws NoSuchElementException
+	 */
+	public E peek() {
+		if(isEmpty()) {
+			throw new NoSuchElementException("Empty Stack");
+		}
+		
+		E data = stack.get(TOP);
+		return data;
+	}
+	
+	public boolean isEmpty() {
+		return stack.size() == 0;
+	}
+	
+	/*
+	 * todo: use iterator
+	 * */
+	@Override
+	public String toString() {
+		return stack.toString();
+	}
+}
