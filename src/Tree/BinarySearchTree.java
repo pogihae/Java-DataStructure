40cac4586da7e3adb276470e6d26342e78446213
PushStone <76048647+pogihae@users.noreply.github.com>
2021-06-19 00:24:04 +0900

    BST non trim

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -7,22 +7,22 @@ public class BinarySearchTree<E extends Comparable<E>> {
 		Node<E> left;
 		Node<E> right;
 		
-		Node() {
-			this(null);
-		}
-		
 		Node(E data) {
 			this.data = data;
 			this.left = null;
 			this.right = null;
 		}
+		
+		@Override
+		public String toString() {
+			return data.toString();
+		}
 	}
 	
-	Node<E> root;
-	
+	private Node<E> root;
 	
 	public BinarySearchTree() {
-		this(null);
+		this.root = null;
 	}
 	
 	public BinarySearchTree(E root) {
@@ -32,25 +32,15 @@ public class BinarySearchTree<E extends Comparable<E>> {
 	public void add(E e) {
 		Node<E> toAdd = new Node<>(e);
 		
-		Node<E> cur = setCur(toAdd);
-		
-		if(cur.data.compareTo(toAdd.data) < 0) {
-			cur.right = toAdd;
-		}
-		else {
-			cur.left = toAdd;
-		}
-	}
-	
-	Node<E> setCur(Node<E> toAdd){
 		Node<E> tmp = root;
-		Node<E> cur = tmp;
+		Node<E> prev = tmp;
+		int cmp = -2;
 		
 		while(tmp != null) {
-			int cmp = tmp.data.compareTo(toAdd.data);
-			cur = tmp;
+			prev = tmp;
+			cmp = tmp.data.compareTo(toAdd.data);
 			
-			if(cmp == -1) {
+			if(cmp < 0) {
 				tmp = tmp.right;
 			}
 			else {
@@ -58,8 +48,108 @@ public class BinarySearchTree<E extends Comparable<E>> {
 			}
 		}
 		
-		return cur;
+		if(cmp == -2) {
+			root = toAdd;
+		}
+		else {
+			if(cmp < 0) {
+				prev.right = toAdd;
+			}
+			else {
+				prev.left = toAdd;
+			}
+		}
 	}
 	
+	public E remove(E e) {
+		//set parent toDel
+		Node<E> pvRoot = new Node<>(null);
+		pvRoot.right = root;
+		
+		Node<E> toDel = root;
+		Node<E> parent = pvRoot;
+		boolean flag = false;
+		
+		while(toDel != null) {
+			int cmp = toDel.data.compareTo(e);
+			
+			if(cmp < 0) {
+				parent = toDel;
+				toDel = toDel.right;
+			}
+			else if(cmp == 0) {
+				flag = true;
+				break;
+			}
+			else {
+				parent = toDel;
+				toDel = toDel.left;
+			}
+		}
+		
+		if(!flag) {
+			return null;
+		}
+		E delData = toDel.data;
+		//remove by childNum
+		while(true) {
+			int childNum = getChildNum(toDel);
+			boolean isLeftChild = (toDel.data.compareTo(parent.data) <= 0)? true:false;
+			
+			switch(childNum) {
+			case 0:
+				if(isLeftChild) {
+					parent.left = null;
+				}
+				else {
+					parent.right = null;
+				}
+				break;
+			case -1: case 1:
+				if(isLeftChild) {
+					parent.left = (childNum == 1)? toDel.left : toDel.right;
+				}
+				else {
+					parent.right = (childNum == 1)? toDel.left : toDel.right;
+				}
+				break;
+			case 2:
+				toDel.data = getLeftBigest(toDel).data;
+				toDel = getLeftBigest(toDel);
+				continue;
+			}
+			break;
+		}
+		
+		return delData;
+	}
 	
+	private int getChildNum(Node<E> n) {
+		if(n.right == null && n.left == null) {
+			return 0;
+		}
+		else if(n.right != null && n.left != null) {
+			return 2;
+		}
+		else if(n.right == null && n.left != null) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	
+	private Node<E> getLeftBigest(Node<E> n) {
+		Node<E> tmp = n;
+		
+		while(tmp.left != null) {
+			tmp = tmp.left;
+		}
+		
+		return tmp;
+	}
+	
+	public void printTree() {
+		
+	}
 }
