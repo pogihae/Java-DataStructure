9608a964456a7e7dc229d3b0505ed8993b061ce9
PushStone <76048647+pogihae@users.noreply.github.com>
2021-08-06 15:44:41 +0900

    Add Hash Table

--- src/symboltable/HashTable.java
+++ src/symboltable/HashTable.java
@@ -1,8 +1,7 @@
 package symboltable;
 
-import list.LinkedList;
-import java.util.Arrays;
-import java.util.function.ToIntFunction;
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
@@ -10,97 +9,66 @@ import java.util.function.ToIntFunction;
  * table using hash key, dynamic array of list
  * 
  * @author tir29
- * @param <T>
  */
-public class HashTable<T> {
-	private LinkedList<T>[] keys;
-	private ToIntFunction<T> hashFunc;
-	
-	public HashTable() {
-		this(10,
-		x -> (int)(x.toString().charAt(0))
-		);
-	}
-	
-	public HashTable(ToIntFunction<T> hashFunc) {
-		this(10, hashFunc);
-	}
-	
-	/**
-	 * constructor
-	 * 
-	 * @param size, for initailize list array
-	 * @param hashFunc, use lambda method
-	 */
-	@SuppressWarnings("unchecked")
-	public HashTable(int size, ToIntFunction<T> hashFunc) {
-		keys = (LinkedList<T>[])(new LinkedList[size]);
-		
-		this.hashFunc = hashFunc;
-	}
-	
-	/**
-	 * 
-	 * insert data
-	 * if data's hashkey is over than array capacity
-	 * increase array until hashkey
-	 * 
-	 * @param data to insert
-	 */
-	public void insert(T data) {
-		//exist keys
-		int key = hashFunc.applyAsInt(data);
-		
-		//non exist
-		if(!isExistKey(key)) {
-			grow(key);
-		}
-		
-		keys[key].add(data);
-	}
-	
-	/**
-	 * @param key that return hash function
-	 * @return true and false, 
-	 * at key in array has list and key is over bound or don't have list
-	 */
-	private boolean isExistKey(int key) {
-		if(key >= keys.length) return false;
-		else if(keys[key] != null) return true;
-		else return false;
-	}
-	
-	/**
-	 * increase array bound with param
-	 * 
-	 * @param key
-	 */
-	private void grow(int key) {
-		//array grow
-		if(key >= keys.length) {
-			keys = Arrays.copyOf(keys, key+1);
-		}
-		
-		//list allocation
-		keys[key] = new LinkedList<>();
-		
-	}
-	
-	/**
-	 * @param data
-	 * @return {@code true} if data is removed {@code ,}
-	 * 			{@code false} otherwise
-	 */
-	public boolean remove(T data) {
-		int key = hashFunc.applyAsInt(data);
-		if(!isExistKey(key)) return false;
-		return keys[key].remove(data);
-	}
-	
-	//search
-	public boolean isExist(T data) {
-		int key = hashFunc.applyAsInt(data);
-		if(!isExistKey(key)) return false;
-		return keys[key].isExist(data);
-	}
+public class HashTable<K extends Comparable<K>, V> {
+    private static class Node<K extends Comparable<K>,V> implements Comparable<Node<K,V>> {
+        K key;
+        V val;
+
+        Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Node<K, V> o) {
+            return key.compareTo(o.key);
+        }
+    }
+
+    List<Node<K,V>>[] table;
+    private final int N;
+    private int size;
+
+    public HashTable() {
+        this(100);
+    }
+
+    public HashTable(int n) {
+        table = new LinkedList[n];
+        N = n;
+        size = 0;
+    }
+
+    private int hashing(K key) {
+        return (key.hashCode() & 0x7fffffff) % N;
+    }
+
+    public void put(K key, V val) {
+        int hc = hashing(key);
+        if(table[hc] == null) table[hc] = new LinkedList<>();
+        table[hc].add(new Node<>(key, val));
+        size++;
+    }
+
+    public V get(K key) {
+        int hc = hashing(key);
+        if(table[hc] == null) return null;
+        return table[hc].stream()
+                .filter(node -> node.key.equals(key))
+                .findAny()
+                .orElse(new Node<>(null, null)).val;
+    }
+
+    public static void main(String[] args) {
+        HashTable<String, Integer> hashTable = new HashTable<>();
+
+        for(int i=0; i<50; i++){
+            hashTable.put("Test"+i, i);
+        }
+
+        System.out.println(hashTable.get("Test"+49));
+    }
+
+
 }
